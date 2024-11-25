370076d6cd51065ac7c386edee56a2d9f8fe4449
invisible-cigarettes <lesteradam03@gmail.com>
2024-11-25 23:46:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import subprocess
 
 subprocess.run(['git', 'reset', '--hard', 'origin/main'], capture_output=True)
-subprocess.run(['git', 'pull'], capture_output=True)
+subprocess.run(['git', 'fetch'], capture_output=True)
 
 
 print('Hello, world! )2')
