efdf9ff909afb8922fb1ae1a3c34a97ba6381033
invisible-cigarettes <lesteradam03@gmail.com>
2024-11-26 00:25:10 +0200

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -3,4 +3,4 @@ import subprocess
 subprocess.run(['git', 'fetch'], capture_output=True)
 subprocess.run(['git', 'reset', '--hard', 'origin/main'], capture_output=True)
 
-from core import ENV
+from main import ENV
