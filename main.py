5cddc87f6f28b45cc8f1d9003c383ed5439812cf
invisible-cigarettes <lesteradam03@gmail.com>
2024-11-26 00:12:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import os
-import sys
 import subprocess
 
 subprocess.run(['git', 'fetch'], capture_output=True)
 subprocess.run(['git', 'reset', '--hard', 'origin/main'], capture_output=True)
 
-# os.execv(sys.executable, [sys.executable] + sys.argv)
-
-# print('Hello (')
 from core import ENV
 print(ENV)
